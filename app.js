8f2f7e212896c24a88f20385b7709fb4afe3d5b7
vishnusingh0101 <singh.vishnu8369@gmail.com>
2023-04-20 18:22:28 +0530

    added payment feature

--- app.js
+++ app.js
@@ -8,22 +8,28 @@ app.use(cors());
 
 const sequelize = require('./util/database');
 
-const dataRoute = require('./routes/getData');
+const dataRoute = require('./routes/expence');
 const userRoute = require('./routes/user');
+const premiumRoute = require('./routes/premium');
 
 const errorControl = require('./controller/error');
 const Expence = require('./model/expence');
 const User = require('./model/user');
+const Order = require('./model/orders');
 
 app.use(bodyParser.json({extended: false}));
 
 app.use(dataRoute);
 app.use('/user', userRoute);
+app.use('/purchase', premiumRoute);
 
 app.use(errorControl.get404);
 
 User.hasMany(Expence);
 Expence.belongsTo(User);
+
+User.hasMany(Order);
+Order.belongsTo(User);
 
 sequelize.sync()
 .then(result => {
---
+++ routes/expence.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+
+const router = express.Router();
+
+const dataControl = require('../controller/expence');
+const authenticator = require('../middleware/auth');
+
+router.get('/allExpence', authenticator.authenticate, dataControl.getAllExpence);
+
+router.post('/addExpence', authenticator.authenticate, dataControl.addExpence);
+
+router.delete('/delete/:id', authenticator.authenticate, dataControl.deleteExpence);
+
+router.post('/edit', authenticator.authenticate, dataControl.postEditExpence);
+
+module.exports = router;
