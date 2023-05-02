c75f1eeec55199c7c52eee019675c150ada826b5
vishnusingh0101 <singh.vishnu8369@gmail.com>
2023-05-02 08:54:28 +0530

    added helmet and morgon

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+
 
 const app = express();
-
-app.use(cors());
+const helmet = require('helmet');
 
 const sequelize = require('./util/database');
+
+const errorControl = require('./controller/error');
 
 const dataRoute = require('./routes/expence');
 const userRoute = require('./routes/user');
 const premiumRoute = require('./routes/premium');
 const passwordRoute = require('./routes/password');
 
-const errorControl = require('./controller/error');
 const Expence = require('./model/expence');
 const User = require('./model/user');
 const Order = require('./model/orders');
 const Report = require('./model/report');
-
 const Forgotpassword = require('./model/password');
 
+const accessLogStream = fs.createWriteStream(
+    path.join(__dirname, 'access.log'),
+    {flags: 'a'}
+);
 
+app.use(cors());
+app.use(helmet());
 app.use(bodyParser.json({extended: false}));
+app.use(morgan('combined', {stream: accessLogStream}));
 
-app.use('/user', dataRoute);
 app.use(userRoute);
+app.use('/user', dataRoute);
 app.use('/premium', premiumRoute);
 app.use('/password', passwordRoute)
 
