1dddca16d5c5fd6b70dbcb1d93ab4711b11a7e87
vishnusingh0101 <singh.vishnu8369@gmail.com>
2023-05-08 18:34:51 +0530

    correction

--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 const Sequalize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequalize('tracker', process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+const sequelize = new Sequalize('expencetracker', process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     dialect: 'mysql',
     host: process.env.DB_HOST
 });
