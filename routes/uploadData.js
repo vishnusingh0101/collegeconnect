8991710852947797a9bc8c11622f717db5dadf07
vishnusingh0101 <singh.vishnu8369@gmail.com>
2025-04-06 12:05:54 +0530

    updates

--- routes/uploadData.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const uploadController = require('../controller/uploaddata.js');
-
-router.post('/uploadcolleges', uploadController.uploadColleges);
-
-module.exports = router;
