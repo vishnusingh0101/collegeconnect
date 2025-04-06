225120a917be0074099c1660d849ad227015fb91
vishnusingh0101 <singh.vishnu8369@gmail.com>
2025-04-06 14:56:19 +0530

    update key secret

--- controller/payment.js
+++ controller/payment.js
@@ -6,7 +6,7 @@ exports.createOrder = async (req, res) => {
     try {
         const razorpay = new Razorpay({
             key_id: process.env.RAZORPAY_KEY_ID,
-            key_secret: process.env.RAZORPAY_SECRET
+            key_secret: process.env.RAZORPAY_KEY_SECRET
         });
         const { amount, currency = "INR" } = req.body;
 
